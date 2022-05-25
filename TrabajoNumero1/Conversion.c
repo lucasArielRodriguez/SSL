0a6df19fc0cb89205469930e6e9e1fedb24aa391
LucasArielRodriguez20 <94145002+LucasArielRodriguez20@users.noreply.github.com>
2022-05-25 17:08:01 -0300

    Delete TrabajoNumero1 directory

--- TrabajoNumero1/Conversion.c
+++
@@ -1,7 +0,0 @@
-#include "Conversion.h"
-double Celsius(double f){
-return (5.0/9.0)*(f-32);
-}
-double Farenheit(double c){
-return (c*(9.0/5.0))+32;
-}
--- TrabajoNumero1/Conversion.h
+++
@@ -1,5 +0,0 @@
-#ifndef CONVERSION_H_INCLUDED
-#define CONVERSION_H_INCLUDED
-double Farenheit(double);
-double Celsius(double);
-#endif
